e379bd9160b4824e76ccc1eb9f6c0cd5d51aaafb
SmokeCircle <82217067+smokecircleteam@users.noreply.github.com>
2022-01-16 17:01:26 -0600

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,8 +1,15 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 
-const firebaseConfig = {
-  // YOUR CONFIG HERE
+const firebaseConfig = {// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  apiKey: "AIzaSyBVw9BV_iPnqeAHpf8L1VMoEEW0U7RkMso",
+  authDomain: "qwitter-a38db.firebaseapp.com",
+  projectId: "qwitter-a38db",
+  storageBucket: "qwitter-a38db.appspot.com",
+  messagingSenderId: "434855552429",
+  appId: "1:434855552429:web:1e25b6bb7857b9d17a2d24",
+  measurementId: "G-ZXHGE7F78B"
 }
 
 firebase.initializeApp(firebaseConfig)
